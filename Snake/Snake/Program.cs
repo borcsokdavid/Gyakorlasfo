83efd4487174bdb7fa84210bc2df3fae624e0b43
borcsokdavid <borcsokdavid@gmail.hu>
2019-12-16 10:03:18 +0100

    feladat

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -14,18 +14,120 @@ namespace Snake
             int magassag = Console.WindowHeight;
             //Console.WriteLine($"{szelesseg} és {magassag}");
 
-            Console.SetCursorPosition(szelesseg / 2, magassag / 2);
-            Console.Write("*");
+            int x = szelesseg / 2;
+            int y = magassag / 2;
 
-            ConsoleKeyInfo cki = Console.ReadKey();
+            int[] xkord = new int[5];
+            int[] ykord = new int[5];
 
-            switch (cki.Key)
+            for (int i = 0; i < 5; i++)
             {
-                default:
+                xkord[i] = x + i;
+                ykord[i] = y;
+
+            }
+
+            megrajzol(xkord, ykord);
+
+
+
+            ConsoleKeyInfo cki = new ConsoleKeyInfo();
+
+            while (cki.Key != ConsoleKey.Escape)
+            {
+
+                cki = Console.ReadKey();
+                switch (cki.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        Console.Clear();
+                        baloldalra(ref xkord, ref ykord);
+                        Console.SetCursorPosition(x, y);
+                        megrajzol(xkord, ykord);
+
+                        break;
+                    case ConsoleKey.UpArrow:
+                        Console.Clear();
+                        felfele(ref xkord, ref ykord);
+                        Console.SetCursorPosition(x, y);
+                        megrajzol(xkord, ykord);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        Console.Clear();
+                        jobboldalra(ref xkord, ref ykord);
+                        Console.SetCursorPosition(x, y);
+                        megrajzol(xkord, ykord);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.Clear();
+                        lefele(ref xkord, ref ykord);
+                        Console.SetCursorPosition(x, y);
+                        megrajzol(xkord, ykord);
+                        break;
+                }
             }
 
 
+
             Console.ReadKey(true);
+        }
+
+        static void megrajzol(int[] x, int[] y)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.SetCursorPosition(x[i], y[i]);
+                Console.Write('*');
+            }
+        }
+        static void lefele(ref int[] x, ref int[] y)
+        {
+            int[] atmenetx = x;
+            int[] atmenety = y;
+            for (int i = 0; i < 4; i++)
+            {
+                x[i] = atmenetx[i + 1];
+                y[i] = atmenety[i + 1];
+            }
+            x[4] = atmenetx[4];
+            y[4] = atmenety[4] + 1;
+        }
+        static void jobboldalra(ref int[] x, ref int[] y)
+        {
+            int[] atmenetx = x;
+            int[] atmenety = y;
+            for (int i = 0; i < 4; i++)
+            {
+                x[i] = atmenetx[i + 1];
+                y[i] = atmenety[i + 1];
+            }
+            x[4] = atmenetx[4] + 1;
+            y[4] = atmenety[4];
+        }
+        static void baloldalra(ref int[] x, ref int[] y)
+        {
+            int[] atmenetx = x;
+            int[] atmenety = y;
+            for (int i = 0; i < 4; i++)
+            {
+                x[i] = atmenetx[i + 1];
+                y[i] = atmenety[i + 1];
+            }
+            x[4] = atmenetx[4] - 1;
+            y[4] = atmenety[4];
+        }
+        static void felfele(ref int[] x, ref int[] y)
+        {
+            int[] atmenetx = x;
+            int[] atmenety = y;
+            for (int i = 0; i < 4; i++)
+            {
+                x[i] = atmenetx[i + 1];
+                y[i] = atmenety[i + 1];
+            }
+            x[4] = atmenetx[4];
+            y[4] = atmenety[4] - 1;
         }
     }
 }
